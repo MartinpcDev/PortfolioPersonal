4b1a0bd6d3659a3470d510155e953944a57f93a0
martin <MartinChrisPC@hotmail.com>
2023-10-07 16:52:59 -0500

    preparando el ambiente

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { NavBar } from './components/NavBar';
+
+export const App = () => {
+	return (
+		<>
+			<NavBar />
+		</>
+	);
+};
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,5 @@
+import { useState } from 'react';
+
+export const NavBar = () => {
+	return <div className='p-3 grid grid-cols-1'>NavBar</div>;
+};
