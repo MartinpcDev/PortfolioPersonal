404a686e678b4bb4688c1abaf5c2a701cf204ae0
martin <MartinChrisPC@hotmail.com>
2023-10-14 20:03:23 -0500

    Fixed Skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,10 +2,7 @@ import { AiOutlineHtml5 } from 'react-icons/ai';
 import { SiTailwindcss } from 'react-icons/si';
 import { BiLogoJavascript, BiLogoCss3, BiLogoReact } from 'react-icons/bi';
 import { BsFillDatabaseFill, BsGit } from 'react-icons/bs';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/autoplay';
+import { ParallaxText } from '../utils/ParallaxText';
 
 export const Skills = () => {
 	const skills = [
@@ -42,13 +39,21 @@ export const Skills = () => {
 	return (
 		<section className='container w-full mx-auto flex px-5 py-24 flex-col items-center justify-center'>
 			<h1 className='text-resaltadoRed text-5xl font-titulos pb-7'>Skills</h1>
-			<div className='flex flex-row justify-center items-center text-center gap-5 flex-wrap duration-300'>
-				{skills.map(skill => (
-					<div className='text-titulos text-tamanotitulos flex flex-col items-center justify-center text-center pr-8 pl-8'>
-						<span className='text-titulos'>{skill.icon}</span>
-						<h1 className='text-parrafos text-tamanotitulos'>{skill.name}</h1>
-					</div>
-				))}
+			<div className='font-titulos text-tamanotitulos gap-8'>
+				<ParallaxText
+					className='flex flex-row overflow-hidden tracking-tight leading-3 m-0 whitespace-nowrap flex-nowrap p-5'
+					baseVelocity={30}>
+					{skills.map(skill => (
+						<span
+							className='flex mr-8 flex-col items-center gap-4'
+							key={skill.name}>
+							<i className='text-titulos font-titulos text-[35px]'>
+								{skill.icon}
+							</i>
+							<h1 className='text-parrafos text-tamanotitulos'>{skill.name}</h1>
+						</span>
+					))}
+				</ParallaxText>
 			</div>
 		</section>
 	);
---
+++ src/utils/ParallaxText.jsx
@@ -0,0 +1,46 @@
+import {
+	motion,
+	useAnimationFrame,
+	useMotionValue,
+	useScroll,
+	useSpring,
+	useTransform,
+	useVelocity,
+	wrap
+} from 'framer-motion';
+import { useRef } from 'react';
+
+export const ParallaxText = ({ children, baseVelocity = 100 }) => {
+	const baseX = useMotionValue(0);
+	const x = useTransform(baseX, v => `${wrap(125, -50, v)}%`);
+	const { scrollY } = useScroll();
+	const scrollVelocity = useVelocity(scrollY);
+	const smoothVelocity = useSpring(scrollVelocity, {
+		damping: 50,
+		stiffness: 400
+	});
+	const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+		clamp: false
+	});
+	const directionFactor = useRef(1);
+	useAnimationFrame((t, delta) => {
+		let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+		if (velocityFactor.get() < 0) {
+			directionFactor.current = -1;
+		} else if (velocityFactor.get() > 0) {
+			directionFactor.current = 1;
+		}
+
+		moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+		baseX.set(baseX.get() + moveBy);
+	});
+	return (
+		<>
+			<motion.div className='flex flex-row' style={{ x }}>
+				{children}
+			</motion.div>
+		</>
+	);
+};
